d645db17cd907c434c835e70f61818c70322f08a
MohamedElabdEmber <62383459+MohamedElabdEmber@users.noreply.github.com>
2021-02-24 14:07:04 +0200

    Delete EMBERPodFramework directory

--- EMBERPodFramework/VideoCall/VideoCallManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  VideoCallViewController.swift
-//  OsirisBio Provider
-//
-//  Created by Pramod More on 8/29/18.
-//  Copyright © 2018 Biz4Solutions. All rights reserved.
-//
-
-import UIKit
